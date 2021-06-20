23b437ebf4f4a099d02397b4066bd9a8c35cdc86
glcos <gianluigi@cosari.it>
2021-06-20 20:23:25 +0200

    Add files via upload

--- ptvo.py
+++ ptvo.py
@@ -2,7 +2,6 @@
 from zigpy.profiles import zha
 from zigpy.quirks import CustomCluster, CustomDevice
 from zhaquirks import Bus, LocalDataCluster
-import zigpy.types as t
 from zigpy.zcl.clusters.homeautomation import Diagnostic
 from zigpy.zcl.clusters.general import Basic, BinaryInput, Identify, AnalogInput, PowerConfiguration, OnOff
 from zigpy.zcl.clusters.measurement import RelativeHumidity, TemperatureMeasurement
@@ -16,23 +15,69 @@ from zhaquirks.const import (
     PROFILE_ID,
 )
 
-PTVO_ANALOG_INPUT_CLUSTER_ID = 0x000C  # decimal = 12
 
-class PtvoCluster(CustomCluster):
-    """PTVO Cluster."""
+TEMPERATURE_REPORTED = "temperature_reported"
+HUMIDITY_REPORTED = "humidity_reported"
 
-    cluster_id = PTVO_ANALOG_INPUT_CLUSTER_ID
-    ep_attribute = "humidity"
+
+class PtvoAnalogInputInputCluster(CustomCluster, AnalogInput):
+    """Analog input cluster, used to relay temperature and humidity to Ptvo cluster."""
+
+    cluster_id = AnalogInput.cluster_id
+
 
     def __init__(self, *args, **kwargs):
         """Init."""
         self._current_state = {}
+        self._current_value = 0
         super().__init__(*args, **kwargs)
 
     def _update_attribute(self, attrid, value):
         super()._update_attribute(attrid, value)
         if value is not None:
-            self.endpoint.device.humidity_bus.listener_event("measured_value", value)
+            if attrid == 85:
+                self._current_value = value * 100
+
+            if attrid == 28:
+                if value == "%":
+                    h_value = self._current_value
+                    self.endpoint.device.humidity_bus.listener_event(HUMIDITY_REPORTED, h_value)
+
+                if value == "C":
+                    t_value = self._current_value
+                    self.endpoint.device.temperature_bus.listener_event(TEMPERATURE_REPORTED, t_value)
+
+
+class HumidityMeasurementCluster(LocalDataCluster, RelativeHumidity):
+    """Humidity measurement cluster to receive reports that are sent to the analog cluster."""
+
+    cluster_id = RelativeHumidity.cluster_id
+    MEASURED_VALUE_ID = 0x0000
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self.endpoint.device.humidity_bus.add_listener(self)
+
+    def humidity_reported(self, value):
+        """Humidity reported."""
+        self._update_attribute(self.MEASURED_VALUE_ID, value)
+
+
+class TemperatureMeasurementCluster(LocalDataCluster, TemperatureMeasurement):
+    """Temperature measurement cluster to receive reports that are sent to the analog cluster."""
+
+    cluster_id = TemperatureMeasurement.cluster_id
+    MEASURED_VALUE_ID = 0x0000
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self.endpoint.device.temperature_bus.add_listener(self)
+
+    def temperature_reported(self, value):
+        """Temperature reported."""
+        self._update_attribute(self.MEASURED_VALUE_ID, value)
 
 
 class ptvoTemperature(CustomDevice):
@@ -40,17 +85,11 @@ class ptvoTemperature(CustomDevice):
 
     def __init__(self, *args, **kwargs):
         """Init device."""
-        #self.temperature_bus = Bus()
+        self.temperature_bus = Bus()
         self.humidity_bus = Bus()
         super().__init__(*args, **kwargs)
 
     signature = {
-        #  "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.EndDevice: 2>, complex_descriptor_available=0, user_descriptor_available=0,
-		#  reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress: 128>,
-		#  manufacturer_code=4447, maximum_buffer_size=80, maximum_incoming_transfer_size=160, server_mask=0, maximum_outgoing_transfer_size=160,
-		#  descriptor_capability_field=<DescriptorCapability.0: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False,
-		#  *is_end_device=True, *is_full_function_device=False, *is_mains_powered=False, *is_receiver_on_when_idle=False, *is_router=False,
-		#  *is_security_capable=False)"
         MODELS_INFO: [("ptvo.info", "ptvo.switch")],
         ENDPOINTS: {
             1: {
@@ -66,7 +105,7 @@ class ptvoTemperature(CustomDevice):
             2: {
                 PROFILE_ID: zha.PROFILE_ID,
                 DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
-                INPUT_CLUSTERS: [PtvoCluster.cluster_id],
+                INPUT_CLUSTERS: [AnalogInput.cluster_id],
                 OUTPUT_CLUSTERS: [],
             }
         },
@@ -79,11 +118,13 @@ class ptvoTemperature(CustomDevice):
                     Basic.cluster_id,
                     Diagnostic.cluster_id,
                     OnOff.cluster_id,
+					TemperatureMeasurementCluster,
+					HumidityMeasurementCluster,
                 ],
                 OUTPUT_CLUSTERS: [Basic.cluster_id, OnOff.cluster_id],
             },
             2: {
-                INPUT_CLUSTERS: [PtvoCluster],
+                INPUT_CLUSTERS: [PtvoAnalogInputInputCluster],
                 OUTPUT_CLUSTERS: [],
             }
         }
