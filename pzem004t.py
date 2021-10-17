926a24909d8ebb1366a66eb5314331da6f6273d9
glcos <gianluigi@cosari.it>
2021-10-17 18:18:50 +0200

    Add files via upload

--- pzem004t.py
+++ pzem004t.py
@@ -3,8 +3,10 @@ from zigpy.profiles import zha
 from zigpy.quirks import CustomCluster, CustomDevice
 from zhaquirks import Bus, LocalDataCluster
 from zigpy.zcl.clusters.homeautomation import Diagnostic, ElectricalMeasurement
-from zigpy.zcl.clusters.general import Basic, BinaryInput, Identify, AnalogInput, PowerConfiguration, OnOff, MultistateValue, MultistateInput
+from zigpy.zcl.clusters.general import Basic, AnalogInput, MultistateValue, MultistateInput
 from zigpy.zcl.clusters.measurement import TemperatureMeasurement
+from zigpy.zcl.clusters.smartenergy import Metering
+
 
 from zhaquirks.const import (
     DEVICE_TYPE,
@@ -15,15 +17,14 @@ from zhaquirks.const import (
     PROFILE_ID,
 )
 
-from homeassistant.const import (
-    ELECTRIC_POTENTIAL_VOLT,
-    ELECTRIC_CURRENT_AMPERE,
-)
-
 TEMPERATURE_REPORTED = "temperature_reported"
 VOLTAGE_REPORTED = "voltage_reported"
 CURRENT_REPORTED = "current_reported"
 POWER_REPORTED = "power_reported"
+FREQUENCY_REPORTED = "frequency_reported"
+POWER_FACTOR_REPORTED = "power_factor_reported"
+ENERGY_REPORTED = "energy_reported"
+INSTANTANEOUS_DEMAND = "instantaneous_demand"
 
 PTVO_DEVICE = 0xfffe
 
@@ -55,17 +56,57 @@ class PtvoAnalogInputCluster(CustomCluster, AnalogInput):
                 if value == "V":
                     """Voltage value."""
                     v_value = self._current_value
-                    self.endpoint.device.voltage_bus.listener_event(VOLTAGE_REPORTED, v_value)
+                    self.endpoint.device.electrical_bus.listener_event(VOLTAGE_REPORTED, v_value)
                     
                 if value == "A":
                     """Current value."""
                     c_value = self._current_value
-                    self.endpoint.device.current_bus.listener_event(CURRENT_REPORTED, c_value)
+                    self.endpoint.device.electrical_bus.listener_event(CURRENT_REPORTED, c_value)
                     
                 if value == "W":
                     """Power value."""
                     p_value = self._current_value
-                    self.endpoint.device.power_bus.listener_event(POWER_REPORTED, p_value)
+                    self.endpoint.device.electrical_bus.listener_event(POWER_REPORTED, p_value)
+                    self.endpoint.device.energy_bus.listener_event(INSTANTANEOUS_DEMAND, p_value)
+                    
+                if value == "Hz":
+                    """Frequency value."""
+                    f_value = self._current_value
+                    self.endpoint.device.electrical_bus.listener_event(FREQUENCY_REPORTED, f_value)
+                    
+                if value == "pf":
+                    """Power Factor value."""
+                    pf_value = self._current_value
+                    self.endpoint.device.electrical_bus.listener_event(POWER_FACTOR_REPORTED, pf_value)
+                    
+                if value == "Wh":
+                    """Energy value."""
+                    e_value = self._current_value
+                    self.endpoint.device.energy_bus.listener_event(ENERGY_REPORTED, e_value)
+
+
+class SmartEnergyCluster(LocalDataCluster, Metering):
+
+    cluster_id = Metering.cluster_id
+    CURRENT_SUMM_DELIVERED_VALUE_ID = 0x0000
+    INSTANTANEOUS_DEMAND_VALUE_ID = 0X0400
+    UNIT_OF_MEASURE_VALUE_ID = 0x0300
+
+    POWER_WATT = 0x0000
+    ENERGY_KILO_WATT_HOUR = 0x0000
+    
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self.endpoint.device.energy_bus.add_listener(self)
+
+    def energy_reported(self, value):
+        """Energy reported."""
+        self._update_attribute(self.CURRENT_SUMM_DELIVERED_VALUE_ID, value)
+        
+    def instantaneous_demand(self, value):
+        """Instantaneous power demand reported."""
+        self._update_attribute(self.INSTANTANEOUS_DEMAND_VALUE_ID, value)
 
 
 class TemperatureMeasurementCluster(LocalDataCluster, TemperatureMeasurement):
@@ -88,49 +129,36 @@ class ElectricalMeasurementCluster(LocalDataCluster, ElectricalMeasurement):
 
     cluster_id = ElectricalMeasurement.cluster_id
     POWER_ID = 0x050B
+    VOLTAGE_ID = 0x0505
+    CURRENT_ID = 0x0508
+    FREQUENCY_ID = 0x0300
+    POWER_FACTOR_ID = 0x0510
 
     def __init__(self, *args, **kwargs):
         """Init."""
         super().__init__(*args, **kwargs)
-        self.endpoint.device.power_bus.add_listener(self)
+        self.endpoint.device.electrical_bus.add_listener(self)
 
     def power_reported(self, value):
         """Power reported."""
         self._update_attribute(self.POWER_ID, value)
-
-
-class ElectricalMeasurementRMSVoltageCluster(LocalDataCluster, ElectricalMeasurement):
-    """Electrical measurement cluster."""
-
-    cluster_id = ElectricalMeasurement.cluster_id
-    VOLTAGE_ID = 0x0505
-
-    def __init__(self, *args, **kwargs):
-        """Init."""
-        super().__init__(*args, **kwargs)
-        self.endpoint.device.voltage_bus.add_listener(self)
-
+        
     def voltage_reported(self, value):
         """Voltage reported."""
         self._update_attribute(self.VOLTAGE_ID, value)
-
-
-class ElectricalMeasurementRMSCurrentCluster(LocalDataCluster, ElectricalMeasurement):
-    """Electrical measurement cluster."""
-
-    cluster_id = ElectricalMeasurement.cluster_id
-    CURRENT_ID = 0x0508
-
-    def __init__(self, *args, **kwargs):
-        """Init."""
-        super().__init__(*args, **kwargs)
-        self.endpoint.device.current_bus.add_listener(self)
-
+        
     def current_reported(self, value):
         """Current reported."""
         self._update_attribute(self.CURRENT_ID, value)
         
+    def frequency_reported(self, value):
+        """Frequency reported."""
+        self._update_attribute(self.FREQUENCY_ID, value)
         
+    def power_factor_reported(self, value):
+        """Power Factor reported."""
+        self._update_attribute(self.POWER_FACTOR_ID, value)
+
 
 class pzem004t(CustomDevice):
     """PZEM-004T Ver 3 based on PTVO firmware."""
@@ -138,10 +166,8 @@ class pzem004t(CustomDevice):
     def __init__(self, *args, **kwargs):
         """Init device."""
         self.temperature_bus = Bus()
-        
-        self.voltage_bus = Bus()
-        self.current_bus = Bus()
-        self.power_bus = Bus()
+        self.electrical_bus = Bus()
+        self.energy_bus = Bus()
         
         super().__init__(*args, **kwargs)
 
@@ -183,15 +209,14 @@ class pzem004t(CustomDevice):
         ENDPOINTS: {
            1: {
                 PROFILE_ID: zha.PROFILE_ID,
-                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                DEVICE_TYPE: PTVO_DEVICE,
                 INPUT_CLUSTERS: [
                     Basic.cluster_id,
                     Diagnostic.cluster_id,
                     PtvoAnalogInputCluster,
                     TemperatureMeasurementCluster,
+                    SmartEnergyCluster,
                     ElectricalMeasurementCluster,
-                    ElectricalMeasurementRMSCurrentCluster,
-                    ElectricalMeasurementRMSVoltageCluster,
                 ],
                 OUTPUT_CLUSTERS: [Basic.cluster_id],
             },
